827352fdfe783bf3f0292a4a64ecefbad6fd2c43
Robertas <retasas.ruskys@gmail.com>
2020-06-02 17:17:41 +0300

    Connected to h2 db

--- src/main/java/blog/models/Post.java
+++ src/main/java/blog/models/Post.java
@@ -1,24 +1,26 @@
 package blog.models;
 
+import javax.persistence.*;
 import java.util.Date;
 
+@Entity
+@Table(name = "posts")
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, length = 300)
     private String title;
+
+    @Lob @Column(nullable = false)
     private String body;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
     private User author;
+
+    @Column(nullable = false)
     private Date date = new Date();
-
-    public Post(Long id, String title, String body, User author) {
-        this.id = id;
-        this.title = title;
-        this.body = body;
-        this.author = author;
-        this.date = date;
-    }
-
-    public Post() {
-    }
 
     public Long getId() {
         return id;
@@ -58,5 +60,25 @@ public class Post {
 
     public void setDate(Date date) {
         this.date = date;
+    }
+
+    public Post() {}
+
+    public Post(Long id, String title, String body, User author) {
+        this.id = id;
+        this.title = title;
+        this.body = body;
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", body='" + body + '\'' +
+                ", author=" + author +
+                ", date=" + date +
+                '}';
     }
 }
--- src/main/java/blog/models/User.java
+++ src/main/java/blog/models/User.java
@@ -1,31 +1,28 @@
 package blog.models;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, length = 30, unique = true)
     private String username;
+
+    @Column(length = 60)
     private String passwordHash;
+
+    @Column(length = 100)
     private String fullName;
 
-    public User(Long id, String username, String fullName) {
-        this.id = id;
-        this.username = username;
-        this.fullName = fullName;
-    }
-    public User() {
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", username='" + username + '\'' +
-                ", passwordHash='" + passwordHash + '\'' +
-                ", fullName='" + fullName + '\'' +
-                '}';
-    }
+    @OneToMany(mappedBy = "author")
+    private Set<Post> posts = new HashSet<Post>();
 
     public Long getId() {
         return id;
@@ -51,7 +48,6 @@ public class User {
         this.passwordHash = passwordHash;
     }
 
-
     public String getFullName() {
         return fullName;
     }
@@ -68,5 +64,28 @@ public class User {
         this.posts = posts;
     }
 
-    private Set<Post> posts = new HashSet<Post>();
+    public User() {
+    }
+
+    public User(String username, String fullName) {
+        this.username = username;
+        this.fullName = fullName;
+    }
+
+    public User(Long id, String username, String fullName) {
+        this.id = id;
+        this.username = username;
+        this.fullName = fullName;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", passwordHash='" + passwordHash + '\'' +
+                ", fullName='" + fullName + '\'' +
+                ", posts=" + posts +
+                '}';
+    }
 }
